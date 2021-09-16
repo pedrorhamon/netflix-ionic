65206241513ea586b616a38ec5452af5a175a045
pedrorhamon <pedrorhamon16@gmail.com>
2021-09-16 00:17:39 -0300

    incrementos basicos inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public stories: Array<any> = new Array<any>();
+  public movies: Array<any> = new Array<any>();
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
+  ngOnInit() { }
+
+  ionViewDidEnter(){
+    this.loadStories();
+    this.loadMovies();
+  }
+
+  loadStories(){
+    this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=net&page=1').subscribe(data =>{
+      const response: any = data;
+
+      this.stories = response.Seach;
+    });
+  }
+
+  loadMovies() {
+    this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=net&page=1').subscribe(data =>{
+      const response: any = data;
+
+      this.movies = response.Seach;
+    });
+  }
 }
